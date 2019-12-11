8b91ea583a5b580b45cac84f755a9bde9eb6fcb8
kupchoy <kup.choy@gmail.com>
2019-12-11 15:31:39 -0500

    Added allauth

--- users/views.py
+++ users/views.py
@@ -7,5 +7,5 @@ from .forms import CustomUserCreationForm
 class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'account/signup.html'
 
