8a5826ee45773f490b96c3e16c104d47cd343bb4
kupchoy <kup.choy@gmail.com>
2019-12-20 15:46:57 -0500

    Added Images

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -171,3 +171,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'admin@djangobookstore.com'
 
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
